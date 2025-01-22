496dfc57d9a3e9b8c7fca963acfc7f8d641f6cdd
ttkhachy <tiana@khachiki.com>
2025-01-22 13:41:14 +1100

    started distributing data into multiple files

---
+++ data_fetch.py
@@ -0,0 +1,19 @@
+import yfinance as yf
+
+
+def get_stock_data(ticker, period):
+    """
+    try:
+
+        ticker = yf.Ticker(ticker_input)
+        current_price(current_price = ticker.info["currentPrice"])
+        data = ticker.history(period=period_slct)
+
+        return current_price, data
+    except:
+        return "Error: Invalid Ticker", None
+    """
+    pass
+
+def get_dividend_data(ticker, period):
+    pass
---
+++ layout.py
@@ -0,0 +1,35 @@
+from dash import dcc, html
+
+def create_layout():
+    return html.Div([
+    html.Div([
+        html.H1("Finance Portfolio Dashboard"),
+        html.P("Search and analyze stock market trends with ease."),
+    ]),
+    html.Div([
+        html.Label("Select Period:"),
+        dcc.Dropdown(id="period",
+                        options=[
+                            {"label": "5 Days", "value": "5d"},
+                            {"label": "1 Month", "value": "1mo"},
+                            {"label": "3 Months", "value": "3mo"},
+                            {"label": "6 Months", "value": "6mo"},
+                            {"label": "1 Year", "value": "1y"},
+                            {"label": "Year to Date", "value": "ytd"},
+                            {"label": "Max", "value": "max"},
+                        ],
+                        value="5d",
+                        multi=False,
+                        style={"width": "40%"}
+
+        ),
+        html.Label("Enter Stock Ticker:"),
+        dcc.Input(id="ticker_input", type="text", placeholder="AAPL", style={'marginRight':'10px'})
+    ], style={"width": "30%", "float": "left", "padding": "20px"}), 
+    html.Div([
+        html.Div(id="current_price", children=[]),
+        dcc.Graph(id="stock_graph", figure={}),
+    ], style={"width": "65%", "float": "right", "padding": "20px"}),
+])
+    pass
+
