2d3353beeb0cf7a2ae0210e91048060d0875446a
ttkhachy <tiana@khachiki.com>
2025-02-04 12:13:29 +1100

    added more to layout and starting to work on portfolio pie chart feature

--- data_fetch.py
+++ data_fetch.py
@@ -1,12 +1,12 @@
 import yfinance as yf
-
+import pandas as pd
 
 def get_stock_data(ticker, period):
     """
     try:
 
         ticker = yf.Ticker(ticker_input)
-        current_price(current_price = ticker.info["currentPrice"])
+        current_price = ticker.info["currentPrice"]
         data = ticker.history(period=period_slct)
 
         return current_price, data
@@ -15,5 +15,23 @@ def get_stock_data(ticker, period):
     """
     pass
 
-def get_dividend_data(ticker, period):
-    pass
+def get_dividend_data(ticker: str, input_period: str):
+    
+    if dividends.empty:
+        print("No dividend data available for the selected ticker.")
+    else:
+        print(dividends)
+
+
+
+
+ticker = yf.Ticker("AAPL")
+print(ticker.info["shortName"])
+dividends = ticker.dividends  # Fetch all available dividend data
+
+df_test = ticker.dividends.reset_index()  # Convert to DataFrame
+#print(df_test)
+
+# Rename columns for clarity
+df_test.columns = ["Date", "Dividend"]
+print(df_test["Date"])
--- layout.py
+++ layout.py
@@ -25,11 +25,19 @@ def create_layout():
         ),
         html.Label("Enter Stock Ticker:"),
         dcc.Input(id="ticker_input", type="text", placeholder="AAPL", style={'marginRight':'10px'})
-    ], style={"width": "30%", "float": "left", "padding": "20px"}), 
+    ], style={"width": "30%", "float": "left", "padding": "20px"}),
+
     html.Div([
         html.Div(id="current_price", children=[]),
         dcc.Graph(id="stock_graph", figure={}),
-    ], style={"width": "65%", "float": "right", "padding": "20px"}),
+    ], style={"width": "65%", "float": "left", "padding": "20px"}),
+
+    html.Div([
+        html.Label("Enter stock name and value, then press submit:"),
+        dcc.Input(id="stock_name", type="text", placeholder="Enter Stock Name:"),
+        dcc.Input(id="stock_amount", type="number", placeholder="Enter Stock Value:"),
+        html.Button('Submit', id='submit-val', n_clicks=0),
+        ## how can i get this to be below the graph?
+    ])
 ])
-    pass
 
